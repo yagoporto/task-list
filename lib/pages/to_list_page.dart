0726a29624824a37a1585400f92583ed660ed51c
Yago Porto <yagosouz8@gmail.com>
2025-09-11 15:07:18 -0300

    Mudança no TextFild e adição do botao +

--- lib/pages/to_list_page.dart
+++ lib/pages/to_list_page.dart
@@ -1,45 +1,43 @@
 import 'package:flutter/material.dart';
 
 class ToDoListPage extends StatelessWidget {
-  ToDoListPage({super.key});
+  const ToDoListPage({super.key});
 
-  final TextEditingController emailController =TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(left:16, right: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "Email: ",
-                  labelStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue
+              Expanded(
+          
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicionar Tarefa',
+                    hintText: 'Estudar flutter',
                   ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                style: TextStyle(
                 ),
               ),
-              ElevatedButton(onPressed: login,
-                child: Text('Entrar'))
+              SizedBox(width: 8,),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  
+                ),
+                child: Text('+',
+                  style: 
+                  TextStyle(fontSize: 35),
+                  ),
+                )
             ],
-          )
+          ),
         ),
-      )
+      ),
     );
-  }
-
-  void login(){
-    String text = emailController.text;
-    print(text);
   }
 }
